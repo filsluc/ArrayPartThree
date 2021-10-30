0ca9d35124ef129bac289cc5dd63d8ff87710dd4
filsluc <87770547+filsluc@users.noreply.github.com>
2021-10-29 21:36:38 -0700

    Add files via upload

---
+++ ArrayPartThree.js
@@ -0,0 +1,38 @@
+function leastToGreatest (arr) {
+    
+  }
+  
+  console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
+
+  function greatestToLeast(arr) {
+    
+  }
+  
+  console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
+
+  function lengthSort(arr) {
+    
+  }
+  
+  console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+
+  function alphabetical(arr) {
+
+}
+
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
+
+function byAge(arr){
+  
+}
+
+console.log(byAge([
+    { name: "Quiet Samurai", age: 22 },
+    { name: "Arrogant Ambassador", age: 100 },
+    { name: "Misunderstood Observer", age: 2 },
+    { name: "Unlucky Swami", age: 77 }
+]));
+[ { name: 'Misunderstood Observer', age: 2 },
+{ name: 'Quiet Samurai', age: 22 },
+{ name: 'Unlucky Swami', age: 77 },
+{ name: 'Arrogant Ambassador', age: 100 } ]
